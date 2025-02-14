16f6bb2ff91bdf461171f654b0aaee6d858b4caf
Diego Tsukayama <diegohidenori.ca@hotmail.com>
2025-02-14 15:52:38 -0500

    Initial Commit

---
+++ FinalProjectTests/FinalProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectTests.swift
+//  FinalProjectTests
+//
+//  Created by Ivanna Bandalak on 2025-02-14.
+//
+
+import Testing
+@testable import FinalProject
+
+struct FinalProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
