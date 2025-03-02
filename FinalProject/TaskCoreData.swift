83f6b812fc6a58f622666af01846c1004dad1c1b
Diego Tsukayama <diegohidenori.ca@hotmail.com>
2025-03-02 17:38:13 -0500

    Initial implementation of Core Data.

---
+++ FinalProject/TaskCoreData.swift
@@ -0,0 +1,14 @@
+//
+//  TaskCoreData.swift
+//  FinalProject
+//
+//  Created by Ivanna Bandalak on 2025-03-02.
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class TaskObject: NSManagedObject {
+
+}
